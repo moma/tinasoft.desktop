81797617e390380fcc3668d780238f560bca5219
Julian bilcke <julian.bilcke@iscpif.fr>
2009-12-16 19:46:48 +0100

    work in progress

---
+++ tina/defaults/preferences/tina.js
@@ -0,0 +1,13 @@
+/* application prefs */
+pref("general.startup.tina",true);
+pref("toolkit.defaultChromeURI", "chrome://tina/content/tina.xul");
+
+/* debugging prefs, see:
+ *   https://developer.mozilla.org/en/Debugging_a_XULRunner_Application
+ */
+pref("browser.dom.window.dump.enabled", true);
+pref("javascript.options.showInConsole", true);
+pref("javascript.options.strict", true);
+pref("nglayout.debug.disable_xul_cache", true);
+pref("nglayout.debug.disable_xul_fastload", true);
+
