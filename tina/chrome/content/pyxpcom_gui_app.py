cea137d3af79240fc687ac84874c62624c681ed7
Julian bilcke <julian.bilcke@iscpif.fr>
2009-12-17 10:54:07 +0100

    minimal tina application now work with basic python modules

--- tina/chrome/content/pyxpcom_gui_app.py
+++
@@ -1,9 +0,0 @@
-import mozutils
-
-def pyxpcom_gui_app_doCommand(event):
-    item_name = event.target.id
-    if item_name == "menu_FileQuitItem":
-        mozutils.doQuit(forceQuit=False)
-    elif item_name == "menu_About":
-        arguments = None
-        window.openDialog("chrome://pyxpcom_gui_app/content/about.xul", "about", "centerscreen,modal", arguments)
--- tina/chrome/icons/default/readme.txt
+++
@@ -1,2 +0,0 @@
-Icons are matched to windows id's automatically, see:
-http://developer.mozilla.org/en/docs/Window_icons
