558d772791e9a386140cda9e0bd2a2289bfbb07c
Julian bilcke <julian.bilcke@iscpif.fr>
2009-12-17 11:28:49 +0100

    added tina/xulrunner

---
+++ tina/xulrunner/defaults/pref/url-classifier.js
@@ -0,0 +1,6 @@
+
+// Maximum size of the sqlite3 cache during an update, in bytes
+//@line 4 "/build/buildd/xulrunner-1.9.1-1.9.1.5+nobinonly/build-tree/mozilla/toolkit/components/url-classifier/pref/url-classifier.js"
+pref("urlclassifier.updatecachemax", 104857600);
+//@line 8 "/build/buildd/xulrunner-1.9.1-1.9.1.5+nobinonly/build-tree/mozilla/toolkit/components/url-classifier/pref/url-classifier.js"
+
---
+++ tina/xulrunner/greprefs/system-greprefs.js
@@ -0,0 +1,6 @@
+//
+// insert your system greprefs here
+//
+
+// pref("general.config.filename", "defaults/syspref/system.cfg");
+
