98658c137e317e9358eea49e8ba1dd1f942e73f5
elishowk <elishowk@nonutc.fr>
2010-02-05 15:17:31 +0100

    preparing tinasoft python integration

--- tina/components/tinasoft.py
+++ tina/components/tinasoft.py
@@ -1,11 +1,18 @@
 # -*- coding: utf-8 -*-
-from tinasoft.data import Engine
+from tinasoft import TinaApp
+
+
+import nltk
+#import numpy
+#import yaml
+#import jsonpickle
+
 from xpcom import components, verbose, COMException, ServerException, nsError
 
-class TinasoftDataRelational():
-    _com_interfaces_ = components.interfaces.nsITinasoftDataRelational
+class TinasoftDataRelational(TinaApp):
+    _com_interfaces_ = components.interfaces.nsITinasoft
     _reg_clsid_ = "{4ff50853-96cb-4eca-b633-43be1833ae90}"
     _reg_contractid_ = "Python.TinasoftDataRelational"
 
     def __init__(self):
-        return Engine("mozstorage://test-tinasoft.sqlite")
+        TinaApp.__init__()
--- tina/pylib/tinasoft/__init__.py
+++ tina/pylib/tinasoft/__init__.py
@@ -1,1 +1,71 @@
 # -*- coding: utf-8 -*-
+__author__="Elias Showk"
+__all__ = ["pytextminer","data"]
+# use this to dispatch tinasoft in multiple packages/directory
+#__import__('pkg_resources').declare_namespace(__name__)
+
+# core modules
+from tinasoft.pytextminer import stopwords, indexer
+from tinasoft.data import Engine
+
+# core utilities
+import os
+import locale
+# command line utility
+from optparse import OptionParser
+import logging
+import logging.handlers
+LOG_FILENAME = 'log/tinasoft.log'
+# configuration file parsing
+import yaml
+
+class TinaApp():
+    """ base class for a tinasoft.pytextminer application"""
+    def __init__(self, configFile='config.yaml', storage=None, loc=None, stopw=None, index=None ):
+        # Set up a specific logger with our desired output level
+        self.logger = logging.getLogger('TinaAppLogger')
+        self.logger.setLevel(logging.DEBUG)
+
+        # Add the log message handler to the logger
+        handler = logging.handlers.RotatingFileHandler(\
+            LOG_FILENAME, maxBytes=1000000, backupCount=5)
+
+        self.logger.addHandler(handler)
+        # import config yaml
+        try:
+            self.config = yaml.safe_load( file( configFile, 'rU' ) )
+        except yaml.YAMLError, exc:
+            self.logger.error( "Unable to read the config.yaml file : " + exc)
+            return
+
+        # tries support of the locale by the host system
+        try:
+            if loc is None:
+                self.locale = self.config['locale']
+            else:
+                self.locale = loc
+            locale.setlocale(locale.LC_ALL, self.locale)
+        except:
+            self.locale = 'en_US.UTF-8'
+            self.logger.error( "locale %s was not found,\
+                switching to en_US.UTF-8 by default"%self.locale)
+            locale.setlocale(locale.LC_ALL, self.locale)
+
+        # load Stopwords object
+        if stopw is None:
+            self.stopwords = stopwords.StopWords( "file://%s" % self.config['stopwords'] )
+        else:
+            self.stopwords = stopwords.StopWords( "file://%s" % stopw )
+
+        # connect sqlite database
+        if storage is None:
+            self.storage = Engine(self.config['storage'])
+        else:
+            self.storage = Engine(storage)
+
+        # connect to text-index
+        if index is None:
+            self.index = indexer.TinaIndex(self.config['index'])
+        else:
+            self.index = indexer.TinaIndex(index)
+
