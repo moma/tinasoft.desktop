5477cbd0e9b382e0ac58aae571cd1255e3c2802c
elishowk <elishowk@nonutc.fr>
2010-02-10 19:15:36 +0100

    TinaApp.importFile and maint.html for importing csv

--- tina/components/tinasoft.py
+++ tina/components/tinasoft.py
@@ -3,42 +3,7 @@ from tinasoft import TinaApp
 
 import sys
 import os
-
-print ""
-print "pythonlib paths:"
-for p in sys.path:
-   print p
-
-print ""
-
-print "exec_prefix:",sys.exec_prefix
-
-print "executable:",sys.executable
-
-
-
 from distutils import sysconfig
-
-pyver = sysconfig.get_config_var('VERSION')
-getvar = sysconfig.get_config_var
-
-if True:
-
-    print "prefix:",sysconfig.PREFIX
-
-    print  "exec_prefix:",sysconfig.EXEC_PREFIX
-
-    flags = ['-I' + sysconfig.get_python_inc(),
-             '-I' + sysconfig.get_python_inc(plat_specific=True)]
-    flags.extend(getvar('CFLAGS').split())
-    print ' '.join(flags)
-
-    libs = getvar('LIBS').split() + getvar('SYSLIBS').split()
-    libs.append('-lpython'+pyver)
-    # add the prefix/lib/pythonX.Y/config dir, but only if there is no
-    # shared library in prefix/lib/.
-    libs.insert(0, '-L' + getvar('LIBPL'))
-    print ' '.join(libs)
 
 from xpcom import components, verbose, COMException, ServerException, nsError
 
@@ -49,3 +14,28 @@ class Tinasoft(TinaApp):
 
     def __init__(self):
         TinaApp.__init__(self)
+
+    def pythonEnv(self):
+        self.logger.debug( "python environment debug:" )
+        for p in sys.path:
+           self.logger.debug( p )
+        self.logger.debug( "exec_prefix:"+sys.exec_prefix )
+        self.logger.debug( "executable:"+sys.executable )
+
+        pyver = sysconfig.get_config_var('VERSION')
+        getvar = sysconfig.get_config_var
+
+        #self.logger.debug( "prefix:"+sysconfig.PREFIX )
+        #self.logger.debug(  "exec_prefix:"+sysconfig.EXEC_PREFIX )
+
+        flags = ['-I' + sysconfig.get_python_inc(),
+                 '-I' + sysconfig.get_python_inc(plat_specific=True)]
+        flags.extend(getvar('CFLAGS').split())
+        self.logger.debug( ' '.join(flags) )
+
+        libs = getvar('LIBS').split() + getvar('SYSLIBS').split()
+        libs.append('-lpython'+pyver)
+        # add the prefix/lib/pythonX.Y/config dir, but only if there is no
+        # shared library in prefix/lib/.
+        libs.insert(0, '-L' + getvar('LIBPL'))
+        self.logger.debug( ' '.join(libs) )
