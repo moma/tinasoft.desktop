ed68db2c6929289459fd44dea74b99f7641cad07
elishowk <elishowk@nonutc.fr>
2010-03-11 18:22:38 +0100

    main.html is more friendly

--- tina/chrome/content/main.js
+++ tina/chrome/content/main.js
@@ -244,7 +244,7 @@ function displayListGraph(trid, corpora) {
             + graphList[i]
             + "'>"
             + graphList[i]
-            + "</button>"
+            + "</button><br/>"
         ).click(function(event) {
             tabvizframe.tinaviz.clear();
             console.log( "opening " + $(this).attr('value') );
