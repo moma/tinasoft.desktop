6b204c2da720122fac899e90530325f1bf8186fb
Julian bilcke <julian.bilcke@iscpif.fr>
2010-03-26 14:58:49 +0100

    preparing to merge old code with new code

---
+++ viz/src/tinaviz/transformations/filters/WeightSize.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tinaviz.transformations.filters;
+
+import java.util.List;
+import tinaviz.Node;
+import tinaviz.transformations.NodeFilter;
+import tinaviz.model.Session;
+import tinaviz.model.View;
+
+/**
+ *
+ * @author jbilcke
+ */
+public class WeightSize extends NodeFilter {
+
+    private String KEY_VALUE = "value";
+
+    @Override
+    public List<Node> process(Session session, View view, List<Node> input) {
+        if (!enabled()) {
+            return input;
+        }
+
+        if (!view.properties.containsKey(root + KEY_VALUE)) {
+            view.properties.put(root + KEY_VALUE, 1.0f);
+        }
+
+        Object o = view.properties.get(root + KEY_VALUE);
+        Float r = (o instanceof Integer)
+                ? new Float((Integer) o)
+                : (o instanceof Double)
+                ? new Float((Double) o)
+                : (Float) o;
+
+        for (Node n : input) {
+            for (String k : n.weights.keySet()) {
+                n.weights.put(k,n.weights.get(k) * r);
+            }
+        }
+        return input;
+    }
+}
