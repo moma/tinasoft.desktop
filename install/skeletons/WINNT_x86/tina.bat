3a3ad3298447386b68903a6e24375fe048ab58b3
julian bilcke <julian.bilcke@gmail.com>
2010-06-18 11:39:26 +0200

    cleaned win32 bat script

--- install/skeletons/WINNT_x86/tina.bat
+++ install/skeletons/WINNT_x86/tina.bat
@@ -1,17 +1,11 @@
 @echo off
 REM echo hello %USERNAME%, you have %NUMBER_OF_PROCESSORS% processors.
-set PLATFORMDIR=%cd%\platform\WINNT_x86-msvc
-set LIBDIR=%PLATFORMDIR%\lib
-set BERKELEYBIN=%PLATFORMDIR%\berkeley\bin
-set APPDATA=%PLATFORMDIR%\AppData
 set JAVA_HOME=%cd%\java
-PATH=%BERKELEYBIN%;%LIBDIR%;%APPDATA%;%JAVA_HOME%;%JAVA_HOME%\lib\
+PATH=%JAVA_HOME%;%JAVA_HOME%\lib\
 set NLTK_DATA=%cd%\shared\nltk_data
-REM echo appdata=%APPDATA%
 REM echo *******************************
-REM echo path=%PATH%
-REM echo *******************************
-REM echo nltk=%NLTK_DATA%
+REM echo PAT=%PATH%
+REM echo NLTK_DATA=%NLTK_DATA%
 REM echo *******************************
 REM  start /max %cd%\xulrunner-stub.exe
 REM xulrunner-stub.exe
