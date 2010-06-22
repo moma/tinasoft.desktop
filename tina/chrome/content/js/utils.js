8c58acd247ed0ac4c1424014102e5f386b4a5430
Julian Bilcke <julian.bilcke@iscpif.fr>
2010-06-22 15:39:00 +0200

    updated code

---
+++ tina/chrome/content/js/utils.js
@@ -0,0 +1,68 @@
+/*
+ * utility modifying the Object prototype
+ * to get its lenght
+ */
+Object.size = function(obj) {
+    var size = 0, key;
+    for (key in obj) {
+        if (obj.hasOwnProperty(key)) size++;
+    }
+    return size;
+};
+/*
+ * utility returning a list
+ * from the values of a given object
+ */
+Object.values = function(obj) {
+    var values = new Array();
+    for (key in obj) {
+        values.push(obj[key]);
+    }
+    return values;
+};
+
+/*
+ * utility to safely decode encoded
+ * values from JSON sent by the java applet
+ */
+function decodeJSON(encvalue) {
+    if (encvalue !== undefined)
+        return decodeURIComponent(encvalue).replace(/\+/g, " ").replace(/%21/g, "!").replace(/%27/g, "'").replace(/%28/g, "(").replace(/%29/g, ")").replace(/%2A/g, "*");
+    else
+        return "";
+};
+
+ /* useful for fullscreen mode */
+function getScreenWidth() {
+    var x = 0;
+    if (self.innerHeight) {
+            x = self.innerWidth;
+    }
+    else if (document.documentElement && document.documentElement.clientHeight) {
+            x = document.documentElement.clientWidth;
+    }
+    else if (document.body) {
+            x = document.body.clientWidth;
+    }
+    return x;
+}
+
+/* useful for fullscreen mode */
+function getScreenHeight() {
+    var y = 0;
+    if (self.innerHeight) {
+        y = self.innerHeight;
+    }
+    else if (document.documentElement && document.documentElement.clientHeight) {
+        y = document.documentElement.clientHeight;
+    }
+    else if (document.body) {
+        y = document.body.clientHeight;
+    }
+
+    return y;
+}
+
+
+
+
