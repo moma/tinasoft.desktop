572650edfff9d8a273e7766a05d6fae715ad35e9
Julian Bilcke <julian.bilcke@gmail.com>
2010-07-22 11:27:38 +0200

    fixed bugs in the mac script

--- install/scripts/build.mac.sh
+++ install/scripts/build.mac.sh
@@ -10,10 +10,8 @@ version="1.0alpha6"
 arch="macosx-10.6"
 
 outpath="TinasoftPytextminer/dist/httpserver.app"
-outpathrs="$outpath/Resources"
+outpathres="$outpath/Resources"
 outfile="$name-$version-$arch"
-
-outpath=
 
 if [ -e $outpath ]
   then
@@ -25,19 +23,18 @@ cd TinasoftPytextminer
 python freeze_mac.py py2app
 cd ..
 
-echo " - copying tinasoft.desktop files.."
-cp -R static/ $outpathres
-cp -R examples/ $outpathres
+echo " - copying tinasoft.desktop files to $outpath.."
+cp -r static $outpathres
+cp -r examples $outpathres
 cp README $outpath
 cp LICENCE $outpath
 cp desktop_config_unix.yaml $outpathres
 
 echo " - creating release archive.."
-find $outpath -name *swp -delete
-find $outpath -name *~ -delete
-find $outpath -name *swo -delete
+#find $outpath -name *swp -delete
+#find $outpath -name *~ -delete
+#find $outpath -name *swo -delete
 
-cd dist
 zip -r $outfile.zip $outpath
 hdiutil create $outfile.dmg -volname "$name $version" -fs HFS+ -srcfolder "$outpath"
 
