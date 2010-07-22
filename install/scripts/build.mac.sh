d43549cd837e17026da96bb40f17ff93c1326a50
jbilcke <jbilcke@bigmac.iscpif.fr>
2010-07-22 11:15:45 +0200

    updating buid.mac file

--- install/scripts/build.mac.sh
+++ install/scripts/build.mac.sh
@@ -1,5 +1,43 @@
-echo "mac"
-cp -R tina dist/tinasoft-mac
-rm -Rf dist/tinasoft-mac/xulrunner
-rm dist/tinasoft-mac/tina # remove the linux executable
-cp -R install/skeletons/mac/* dist/tinasoft-mac/.
+#/bin/bash
+
+echo "############################################"
+echo "# BUILD TINASOFT FOR SNOW LEOPARD PLATFORM #"
+echo "############################################"
+echo ""
+
+name="Tinasoft"
+version="1.0alpha6"
+arch="macosx-10.6"
+
+outpath="TinasoftPytextminer/dist/httpserver.app"
+outpathrs="$outpath/Resources"
+outfile="$name-$version-$arch"
+
+outpath=
+
+if [ -e $outpath ]
+  then
+    rm -rf $outpath
+fi
+
+echo " - freezing TinasoftPytextminer.."
+cd TinasoftPytextminer
+python freeze_mac.py py2app
+cd ..
+
+echo " - copying tinasoft.desktop files.."
+cp -R static/ $outpathres
+cp -R examples/ $outpathres
+cp README $outpath
+cp LICENCE $outpath
+cp desktop_config_unix.yaml $outpathres
+
+echo " - creating release archive.."
+find $outpath -name *swp -delete
+find $outpath -name *~ -delete
+find $outpath -name *swo -delete
+
+cd dist
+zip -r $outfile.zip $outpath
+hdiutil create $outfile.dmg -volname "$name $version" -fs HFS+ -srcfolder "$outpath"
+
