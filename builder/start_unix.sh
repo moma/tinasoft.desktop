7f066bd09bb2b71077e0770c51b2864cb3b283ca
elishowk <elishowk@nonutc.fr>
2010-09-20 16:30:07 +0200

    updated README and config_xx.yaml

--- builder/start_unix.sh
+++ builder/start_unix.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export NLTK_DATA=shared/nltk_data
-TinasoftPytextminer/httpserver desktop_config_unix.yaml &
+TinasoftPytextminer/httpserver config_unix.yaml &
 sleep 5
 firefox http://localhost:8888
--- start_unix.sh
+++ start_unix.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 export NLTK_DATA=shared/nltk_data
 python TinasoftPytextminer/httpserver.py TinasoftPytextminer/config_unix.yaml
+sleep 5
+firefox http://localhost:8888
