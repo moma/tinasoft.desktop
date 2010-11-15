037892f22ea6e924302653993d025b1e0c5d1dfa
Julian Bilcke <julian.bilcke@iscpif.fr>
2010-11-15 14:28:06 +0100

    initial commit for slides

--- start_win.bat
+++
@@ -1,5 +0,0 @@
-set NLTK_DATA=shared\nltk_data
-PATH C:\Python26;%PATH%
-python TinasoftPytextminer\httpserver.py TinasoftPytextminer/config_win.yaml
-@ping 127.0.0.1 -n 5 -w 1000 > nul
-start http://localhost:8888
