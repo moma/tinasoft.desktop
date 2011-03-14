ebe399762b7943d23524d0fcb8c32f1d9574188d
elishowk <elishowk@nonutc.fr>
2011-03-14 12:50:32 +0100

    version upgraded

--- builder/scripts/build.win.sh
+++ builder/scripts/build.win.sh
@@ -5,7 +5,7 @@ echo "#############################################"
 echo ""
 sleep 2
 name="Tinasoft"
-version="1.0alpha8"
+version="1.0beta"
 arch="WIN32"
 outfile="$name-$version-$arch"
 outpath="dist/$outfile"
