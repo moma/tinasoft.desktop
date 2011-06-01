f21af7f5b5fcf47bdd5b6a1dfe6442cf36f0147b
Julian Bilcke <julian.bilcke@iscpif.fr>
2011-06-01 19:11:31 +0200

    updated master

--- start_unix.sh
+++ start_unix.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 export NLTK_DATA=shared/nltk_data
-python TinasoftPytextminer/httpserver.py TinasoftPytextminer/config_unix.yaml &
+# was previously only "python", but it is preferrable to specify the version system
+python2.6 TinasoftPytextminer/httpserver.py TinasoftPytextminer/config_unix.yaml &
